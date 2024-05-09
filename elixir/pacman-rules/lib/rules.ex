77b75722f95beb8a7b67f9d5330e0ccd594da24d
Thiago Alencar <talencar@gmail.com>
2024-05-09 12:19:35 -0300

    elixir: Pacman Rules exercise #2

--- elixir/pacman-rules/lib/rules.ex
+++ elixir/pacman-rules/lib/rules.ex
@@ -1,14 +1,18 @@
 defmodule Rules do
-  def eat_ghost?(power_pellet_active?, touching_ghost?) do
-    power_pellet_active? and touching_ghost?
-  end
-  def score?(touching_power_pellet?, touching_dot?) do
-    touching_power_pellet? or touching_dot?
-  end
-  def lose?(power_pellet_active?, touching_ghost?) do
-    not power_pellet_active? and touching_ghost?
-  end
+  def eat_ghost?(power_pellet_active?, touching_ghost?)
+  def eat_ghost?(true, true), do: true
+  def eat_ghost?(_, _), do: false
+
+  def score?(touching_power_pellet?, touching_dot?)
+  def score?(true, false), do: true
+  def score?(false, true), do: true
+  def score?(_, _), do: false
+
+  def lose?(power_pellet_active?, touching_ghost?)
+  def lose?(false, true), do: true
+  def lose?(_, _), do: false
+
   def win?(has_eaten_all_dots?, power_pellet_active?, touching_ghost?) do
-    has_eaten_all_dots? and not lose?(power_pellet_active?, touching_ghost?)
+    has_eaten_all_dots? |> Kernel.and(not lose?(power_pellet_active?, touching_ghost?))
   end
 end
