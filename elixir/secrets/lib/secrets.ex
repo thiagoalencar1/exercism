6602887e0fcb86d81679eefb107253f52a8d56e7
Thiago Alencar <talencar@gmail.com>
2024-05-13 09:57:56 -0300

    elixir: Secrets exercise, solution #3

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,17 +1,9 @@
 defmodule Secrets do
-  def secret_add(secret), do: fn(number) -> number + secret end
-
-  def secret_subtract(secret), do: fn(number) -> number - secret end
-
-  def secret_multiply(secret), do: fn(number) -> number * secret end
-
-  def secret_divide(secret), do: fn(number) -> Integer.floor_div(number, secret) end
-
-  def secret_and(secret), do: fn(number) -> Bitwise.band(number, secret) end
-
-  def secret_xor(secret), do: fn(number) -> Bitwise.bxor(number, secret) end
-
-  def secret_combine(secret_function1, secret_function2) do
-    fn(number) -> secret_function2.(secret_function1.(number)) end
-  end
+  def secret_add(secret), do: &(&1 + secret)
+  def secret_subtract(secret), do: &(&1 - secret)
+  def secret_multiply(secret), do: &(&1 * secret)
+  def secret_divide(secret), do: &(Integer.floor_div(&1, secret))
+  def secret_and(secret), do: &(Bitwise.band(&1, secret))
+  def secret_xor(secret), do: &(Bitwise.bxor(&1, secret))
+  def secret_combine(secret_function1, secret_function2), do: &(secret_function2.(secret_function1.(&1)))
 end
