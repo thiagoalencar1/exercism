a2b981856cb031b1ba8d03ab73417611aa42ac6b
Thiago Alencar <talencar@gmail.com>
2024-05-14 09:55:55 -0300

    elixir: Log Level exercise, solution #2

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,24 +1,22 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
       cond do
-        level == 0 && !legacy? -> :trace
-        level == 0 && legacy? -> :unknown
+        level == 0 and not legacy? -> :trace
         level == 1 -> :debug
         level == 2 -> :info
         level == 3 -> :warning
         level == 4 -> :error
-        level == 5 && !legacy? -> :fatal
-        level == 5 && legacy? -> :unknown
+        level == 5 and not legacy? -> :fatal
         true -> :unknown
     end
   end
 
   def alert_recipient(level, legacy?) do
+    label = to_label(level, legacy?)
     cond do
-      legacy? && LogLevel.to_label(level, legacy?) == :unknown -> :dev1
-      !legacy? && LogLevel.to_label(level, legacy?) == :unknown -> :dev2
-      LogLevel.to_label(level, legacy?) == :fatal -> :ops
-      LogLevel.to_label(level, legacy?) == :error -> :ops
+      label == :unknown and legacy? -> :dev1
+      label == :unknown and not legacy? -> :dev2
+      label == :fatal or label == :error -> :ops
       true -> false
     end
   end
