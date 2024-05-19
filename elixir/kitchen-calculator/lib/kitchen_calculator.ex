81b80d4ee63900723d4e2e058c2bb420ac6947ab
Thiago Alencar <talencar@gmail.com>
2024-05-19 11:28:24 -0300

    elixir: Kitchen Calculator exercise, solution #2

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,31 +1,17 @@
 defmodule KitchenCalculator do
-  def get_volume(volume_pair), do: elem(volume_pair, 1)
+  def get_volume({_unit, volume}), do: volume
 
-  def to_milliliter(volume_pair) do
-    case elem(volume_pair, 0) do
-      :mL           -> {:milliliter, get_volume(volume_pair)}
-      :cup          -> {:milliliter, get_volume(volume_pair) * 240}
-      :fluid_ounce  -> {:milliliter, get_volume(volume_pair) * 30}
-      :teaspoon     -> {:milliliter, get_volume(volume_pair) * 5}
-      :tablespoon   -> {:milliliter, get_volume(volume_pair) * 15}
-      :milliliter   -> {:milliliter, get_volume(volume_pair)}
-    end
-  end
+  def to_milliliter({:cup, volume}), do: {:milliliter, volume * 240}
+  def to_milliliter({:fluid_ounce, volume}), do: {:milliliter, volume * 30}
+  def to_milliliter({:teaspoon, volume}), do: {:milliliter, volume * 5}
+  def to_milliliter({:tablespoon, volume}), do: {:milliliter, volume * 15}
+  def to_milliliter({:milliliter, volume}), do: {:milliliter, volume}
 
-  def from_milliliter(volume_pair, unit) do
-    case unit do
-      :mL           -> {:mL, get_volume(volume_pair)}
-      :cup          -> {:cup, get_volume(volume_pair) / 240}
-      :fluid_ounce  -> {:fluid_ounce, get_volume(volume_pair) / 30}
-      :teaspoon     -> {:teaspoon, get_volume(volume_pair) / 5}
-      :tablespoon   -> {:tablespoon, get_volume(volume_pair) / 15}
-      :milliliter   -> {:milliliter, get_volume(volume_pair)}
-    end
-  end
+  def from_milliliter({:milliliter, volume}, :cup), do: {:cup, volume / 240}
+  def from_milliliter({:milliliter, volume}, :fluid_ounce), do: {:fluid_ounce, volume / 30}
+  def from_milliliter({:milliliter, volume}, :teaspoon), do: {:teaspoon, volume / 5}
+  def from_milliliter({:milliliter, volume}, :tablespoon), do: {:tablespoon, volume / 15}
+  def from_milliliter({:milliliter, volume}, :milliliter), do: {:milliliter, volume}
 
-  def convert(volume_pair, unit) do
-    volume_pair
-    |> to_milliliter()
-    |> from_milliliter(unit)
-  end
+  def convert(vol_pair, unit), do: vol_pair |> to_milliliter() |> from_milliliter(unit)
 end
