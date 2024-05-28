50acd726600321dd039ff919f9676578c3499b9e
Thiago Alencar <talencar@gmail.com>
2024-05-28 09:03:39 -0300

    elixir: Bird Count exercise, solution #1 (whithout recursion)

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,25 @@
 defmodule BirdCount do
+  def today([]), do: nil
   def today(list) do
-    # Please implement the today/1 function
+    [head | _tail] = list
+    head
   end
 
+  def increment_day_count([]), do: [1]
   def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+    [head | tail] = list
+    head = head + 1
+    [head | tail]
   end
 
   def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+    Enum.member?(list, 0)
   end
 
-  def total(list) do
-    # Please implement the total/1 function
-  end
+  def total([]), do: 0
+  def total(list), do: Enum.reduce(list, fn(x, acc) -> x + acc end)
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  def busy_days([]), do: 0
+  def busy_days([1]), do: 0
+  def busy_days(list), do: Enum.count(list, fn(x) -> x >= 5 end)
 end
