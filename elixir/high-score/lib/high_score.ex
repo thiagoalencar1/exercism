d4761e12d4ea6ecb2a90e46f56e5e692fa03082f
Thiago Alencar <talencar@gmail.com>
2024-05-29 16:34:45 -0300

    elixir: High Score exercise, solution #2

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,5 +1,6 @@
 defmodule HighScore do
   @initial_score 0
+
   def new(), do: %{}
 
   def add_player(scores, name, score \\ @initial_score), do: Map.put(scores, name, score)
@@ -8,15 +9,8 @@ defmodule HighScore do
 
   def reset_score(scores, name), do: Map.put(scores, name, @initial_score)
 
-  def update_score(scores, name, score) do
-    scores
-    |> Map.get(name)
-    |> case do
-        nil -> Map.put(scores, name, score)
-        _ -> Map.update!(scores, name, &(&1 + score))
-    end
-  end
-
+  def update_score(scores, name, score) when scores == %{}, do: Map.put(scores, name, score)
+  def update_score(scores, name, score), do: Map.update!(scores, name, &(&1 + score))
 
   def get_players(scores), do: Map.keys(scores)
 end
