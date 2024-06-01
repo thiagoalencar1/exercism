16384d85703c6f0ec00b48cd4d3927de28be4012
Thiago Alencar <talencar@gmail.com>
2024-05-31 23:27:52 -0300

    elixir: German Sysadmin exercise, solution #1 (with improvements)

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -3,12 +3,12 @@ defmodule Username do
 
   def sanitize([head | tail]) do
     case head do
-      head when head in ?a..?z -> [head] ++ sanitize(tail)
-      head when head == ?_ -> [head] ++ sanitize(tail)
-      head when head == ?ö -> [?o,?e] ++ sanitize(tail)
-      head when head == ?ä -> [?a,?e] ++ sanitize(tail)
-      head when head == ?ü -> [?u,?e] ++ sanitize(tail)
-      head when head == ?ß -> [?s,?s] ++ sanitize(tail)
+      head when head in ?a..?z -> [head | sanitize(tail)]
+      head when head == ?_ -> [head | sanitize(tail)]
+      head when head == ?ö -> [?o,?e | sanitize(tail)]
+      head when head == ?ä -> [?a,?e | sanitize(tail)]
+      head when head == ?ü -> [?u,?e | sanitize(tail)]
+      head when head == ?ß -> [?s,?s |  sanitize(tail)]
       _ -> sanitize(tail)
     end
   end
