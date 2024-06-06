b56595df245fb2aafa5f1d8957ecb528d331de02
Thiago Alencar <talencar@gmail.com>
2024-06-06 08:55:38 -0300

    elixir: Take-A-Number exercise, solution #1

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,16 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> loop(0) end)
+  end
+
+  def loop(state) do
+    receive do
+      {:report_state, sender_pid} -> send(sender_pid, state)
+        loop(state)
+      {:take_a_number, sender_pid} -> send(sender_pid, state + 1)
+        loop(state + 1)
+      :stop -> nil
+      _ -> loop(state)
+    end
   end
 end
