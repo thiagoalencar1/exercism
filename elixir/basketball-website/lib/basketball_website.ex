c9403be8df918756631f6e3294e8fb185224118d
Thiago Alencar <talencar@gmail.com>
2024-07-03 08:03:17 +0300

    elixir: Basketball Website, extract_from_path function

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,6 +1,11 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    do_extract_from_path(data, String.split(path, ".")) |> dbg()
+  end
+
+  defp do_extract_from_path(data, []), do: data
+  defp do_extract_from_path(data, [key | rest]) do
+    do_extract_from_path(data[key], rest)
   end
 
   def get_in_path(data, path) do
